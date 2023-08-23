eb4cc50c2751591c1c4b97a8334676b90a001448
Volodymyr Baryshenskiy <vbaryshenskiy@gmail.com>
2023-08-23 17:36:31 -0400

    Added actual "game" There is an actual game. Still needs user input added

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const myArray = ["rock", "paper", "scissors"];
-
+const myArray = ["Rock", "Paper", "Scissors"];
+const resultMessage = ["You win!", "You tie!", "You Lose"];
+const resultCondition = ["Rock beats Scissors", "Paper beats Rock", "Scissors beats Paper", "You both picked the same thing"];
 
 function getComputerChoice() {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
+
+function playRound(playerSelection, computerSelection = getComputerChoice()){
+    let message;
+    if (playerSelection === "Rock" & computerSelection === "Scissors") {
+        message = `${resultMessage[0]} ${resultCondition[0]}`;
+        return message;
+    } else if (playerSelection === "Rock" & computerSelection === "Paper") {
+        message = `${resultMessage[2]} ${resultCondition[1]}`;
+        return message;
+    } else if (playerSelection === "Rock" & computerSelection === "Rock") {
+        message = `${resultMessage[1]} ${resultCondition[3]}`;
+        return message;
+    } else if (playerSelection === "Paper" & computerSelection === "Rock") {
+        message = `${resultMessage[0]} ${resultCondition[1]}`;
+        return message;
+    } else if (playerSelection === "Paper" & computerSelection === "Scissors") {
+        message = `${resultMessage[2]} ${resultCondition[0]}`;
+        return message;
+    } else if (playerSelection === "Paper" & computerSelection === "Paper") {
+        message = `${resultMessage[1]} ${resultCondition[3]}`;
+        return message;
+    } else if (playerSelection === "Scissors" & computerSelection === "Paper") {
+        message = `${resultMessage[0]} ${resultCondition[0]}`;
+        return message;
+    }else if (playerSelection === "Scissors" & computerSelection === "Rock"){
+        message = `${resultMessage[2]} ${resultCondition[0]}`;
+        return message;
+    } else if (playerSelection === "Scissors" & computerSelection === "Paper") {
+        message = `${resultMessage[1]} ${resultCondition[3]}`;
+        return message;
+    }
+}
