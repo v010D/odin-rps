e6127eec3f61f3c2122d90f2929cc107e1f64ffc
Volodymyr Baryshenskiy <vbaryshenskiy@gmail.com>
2023-08-23 23:04:31 -0400

    Added User interaction! Browser will ask user for an input, and will not accept a wrong input

--- index.js
+++ index.js
@@ -6,35 +6,57 @@ function getComputerChoice() {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
+function capitalizeFirstLetter(inputString) {
+    const lowercaseString = inputString.toLowerCase();
+    const capitalizedString = lowercaseString.charAt(0).toUpperCase() + lowercaseString.slice(1);
+    return capitalizedString;
+  }
+  
 
-function playRound(playerSelection, computerSelection = getComputerChoice()){
+let validAnswer = true;
+let playerPick = prompt("Pick one: Rock, Paper, or Scissors");
+playerPick = capitalizeFirstLetter(playerPick);
+
+while (true){
+    if (myArray.includes(playerPick)){
+        break;
+    }
+    playerPick = prompt("From one of the three pick: Rock, Paper, or Scissors");
+    playerPick = capitalizeFirstLetter(playerPick);
+}
+
+console.log(playerPick);
+
+function playRound(playerSelection = playerPick, computerSelection = getComputerChoice()){
     let message;
-    if (playerSelection === "Rock" & computerSelection === "Scissors") {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
         message = `${resultMessage[0]} ${resultCondition[0]}`;
         return message;
-    } else if (playerSelection === "Rock" & computerSelection === "Paper") {
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         message = `${resultMessage[2]} ${resultCondition[1]}`;
         return message;
-    } else if (playerSelection === "Rock" & computerSelection === "Rock") {
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
         message = `${resultMessage[1]} ${resultCondition[3]}`;
         return message;
-    } else if (playerSelection === "Paper" & computerSelection === "Rock") {
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         message = `${resultMessage[0]} ${resultCondition[1]}`;
         return message;
-    } else if (playerSelection === "Paper" & computerSelection === "Scissors") {
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         message = `${resultMessage[2]} ${resultCondition[0]}`;
         return message;
-    } else if (playerSelection === "Paper" & computerSelection === "Paper") {
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
         message = `${resultMessage[1]} ${resultCondition[3]}`;
         return message;
-    } else if (playerSelection === "Scissors" & computerSelection === "Paper") {
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         message = `${resultMessage[0]} ${resultCondition[0]}`;
         return message;
-    }else if (playerSelection === "Scissors" & computerSelection === "Rock"){
+    }else if (playerSelection === "Scissors" && computerSelection === "Rock"){
         message = `${resultMessage[2]} ${resultCondition[0]}`;
         return message;
-    } else if (playerSelection === "Scissors" & computerSelection === "Paper") {
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         message = `${resultMessage[1]} ${resultCondition[3]}`;
         return message;
     }
 }
+
+console.log(playRound(playerPick));
